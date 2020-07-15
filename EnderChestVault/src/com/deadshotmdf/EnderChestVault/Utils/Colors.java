41901a0ab1000ed851e714c88452895a50e94e43
BuildTools <unconfigured@null.spigotmc.org>
2020-07-16 01:14:54 +0300

    Slight modification to the Colors class

--- EnderChestVault/src/com/deadshotmdf/EnderChestVault/Utils/Colors.java
+++ EnderChestVault/src/com/deadshotmdf/EnderChestVault/Utils/Colors.java
@@ -1,7 +1,5 @@
 package com.deadshotmdf.EnderChestVault.Utils;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.bukkit.ChatColor;
 
@@ -9,14 +7,6 @@ public class Colors {
 	
 	public static String color(String s) {
 		return ChatColor.translateAlternateColorCodes('&', s);
-	}
-	
-	public static List<String>color(List<String> s){
-		List<String> l = new ArrayList<String>();
-		for(String ss : s) {
-			l.add(color(ss));
-		}
-		return l;
 	}
 
 }
