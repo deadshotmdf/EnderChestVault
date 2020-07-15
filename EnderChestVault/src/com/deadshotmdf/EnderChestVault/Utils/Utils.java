763da95169c59bf10887de3076f42b27fc63e054
BuildTools <unconfigured@null.spigotmc.org>
2020-07-16 01:09:45 +0300

    Slight modification to the Utils class

--- EnderChestVault/src/com/deadshotmdf/EnderChestVault/Utils/Utils.java
+++ EnderChestVault/src/com/deadshotmdf/EnderChestVault/Utils/Utils.java
@@ -55,11 +55,6 @@ public class Utils {
 		List<ItemStack> l = new ArrayList<ItemStack>();
 		if(main.getPlayersConfig().getConfigurationSection("Players") == null || !main.getPlayersConfig().getConfigurationSection("Players").contains(player.getUniqueId().toString()) || !main.getPlayersConfig().getConfigurationSection("Players." + player.getUniqueId()).contains(String.valueOf(id))) return l;
 		for(String items : main.getPlayersConfig().getConfigurationSection("Players." + player.getUniqueId() + "." + id).getKeys(false)) {
-//			Material mat = Material.getMaterial(main.getPlayersConfig().getString("Players." + player.getUniqueId() + "." + id + "." + items + ".material"));
-//			String name = main.getPlayersConfig().getString("Players." + player.getUniqueId() + "." + id + "." + items + ".name");
-//			List<String> lore = main.getPlayersConfig().getStringList("Players." + player.getUniqueId() + "." + id + "." + items + ".lore");
-//			
-//			l.add(ItemUtil.createItem(mat, name, lore));
 			if(main.getPlayersConfig().getItemStack("Players." + player.getUniqueId() + "." + id + "." + items) == null) continue;
 			ItemStack item = main.getPlayersConfig().getItemStack("Players." + player.getUniqueId() + "." + id + "." + items);
 			l.add(item);
@@ -75,25 +70,6 @@ public class Utils {
 			if(i >= 45) continue;
 			if(inv.getItem(i) == null || inv.getItem(i).getType() == Material.AIR) continue;
 			ItemStack item = inv.getItem(i);
-//			String name = null;
-//			List<String> lore = new ArrayList<String>();
-//			
-//			if(!inv.getItem(i).hasItemMeta() || !inv.getItem(i).getItemMeta().hasDisplayName()) {
-//				net.minecraft.server.v1_8_R3.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
-//				name = nmsStack.getItem().a(nmsStack);
-//			}
-//			else {
-//				name = Colors.color(item.getItemMeta().getDisplayName());
-//			}
-//
-//			
-//			if(inv.getItem(i).getItemMeta().hasLore()) {
-//				lore = Colors.color(item.getItemMeta().getLore());
-//			}
-//			
-//			main.getPlayersConfig().set("Players." + player.getUniqueId() + "." + id + "." + item + i + ".material", item.getType().toString());
-//			main.getPlayersConfig().set("Players." + player.getUniqueId() + "." + id + "." + item + i + ".name", name);
-//			main.getPlayersConfig().set("Players." + player.getUniqueId() + "." + id + "." + item + i + ".lore", lore);
 			main.getPlayersConfig().set("Players." + player.getUniqueId() + "." + id + "." + item + i, item);
 		}
 		saveF();
